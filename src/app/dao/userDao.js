43c184c0aca770f949d3171ac58135e166b8f8d9
Lee Sang Min <dltkd96als@gmail.com>
2021-01-15 23:35:05 +0900

    [Add] 회원가입 API 생성 및 유효성 검사
    추후 레벨 관련 transaction 처리 필요

--- src/app/dao/userDao.js
+++ src/app/dao/userDao.js
@@ -1,73 +1,66 @@
+const { response } = require("express");
 const { pool } = require("../../../config/database");
 
-// Signup
-async function userEmailCheck(email) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectEmailQuery = `
-                SELECT email, nickname 
-                FROM UserInfo 
-                WHERE email = ?;
-                `;
-  const selectEmailParams = [email];
-  const [emailRows] = await connection.query(
-    selectEmailQuery,
-    selectEmailParams
-  );
-  connection.release();
+exports.checkUserEmail = async function (email) {
+    try {
+        const connection = await pool.getConnection(async (conn) => conn);
+        const query = `
+        select exists(select email from User where email = ? and isDeleted = 'N') as exist;
+        `;
+        const params = [email];
+        const [rows] = await connection.query(
+            query, params
+        );
+        connection.release();
 
-  return emailRows;
+        return rows[0]['exist'];
+    } catch (err) {
+        logger.error(`App - checkUserEmail DB Connection error\n: ${err.message}`);
+        return res.status(500).send(`Error: ${err.message}`);
+    }
 }
 
-async function userNicknameCheck(nickname) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectNicknameQuery = `
-                SELECT email, nickname 
-                FROM UserInfo 
-                WHERE nickname = ?;
-                `;
-  const selectNicknameParams = [nickname];
-  const [nicknameRows] = await connection.query(
-    selectNicknameQuery,
-    selectNicknameParams
-  );
-  connection.release();
-  return nicknameRows;
+exports.checkUserNickname = async function (nickname) {
+    try {
+        const connection = await pool.getConnection(async (conn) => conn);
+        const query = `
+        select exists(select nickname from User where nickname = ? and isDeleted = 'N') as exist;
+        `;
+        const params = [nickname];
+        const [rows] = await connection.query(
+            query, params
+        );
+        connection.release();
+
+        return rows[0]['exist'];
+    } catch (err) {
+        logger.error(`App - checkUserNickname DB Connection error\n: ${err.message}`);
+        return res.status(500).send(`Error: ${err.message}`);
+    }
 }
 
-async function insertUserInfo(insertUserInfoParams) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const insertUserInfoQuery = `
-        INSERT INTO UserInfo(email, pswd, nickname)
-        VALUES (?, ?, ?);
-    `;
-  const insertUserInfoRow = await connection.query(
-    insertUserInfoQuery,
-    insertUserInfoParams
-  );
-  connection.release();
-  return insertUserInfoRow;
+// 레벨 부여되도록 트랜잭션 처리 필요
+exports.postUserInfo = async function (name, email, password, nickname, height, weight, gender) {
+    try {
+        const connection = await pool.getConnection(async (conn) => conn);
+        try {
+            console.log(typeof(gender));
+            const query = `
+            insert into User (userName, email, password, nickname, height, weight, gender) values (?, ?, ?, ?, ?, ?, ?);
+            `
+            const params = [name, email, password, nickname, height, weight, gender];
+            const [rows] = await connection.query(
+                query, params
+            );
+            connection.release();
+        } catch (err) {
+            await connection.rollback();
+            connection.release();
+            logger.error(`App - postUserInfo Transaction error\n: ${err.message}`);
+            return res.status(500).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - postUserInfo DB Connection error\n: ${err.message}`);
+        return res.status(500).send(`Error: ${err.message}`);
+    }
 }
-
-//SignIn
-async function selectUserInfo(email) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectUserInfoQuery = `
-                SELECT id, email , pswd, nickname, status 
-                FROM UserInfo 
-                WHERE email = ?;
-                `;
-
-  let selectUserInfoParams = [email];
-  const [userInfoRows] = await connection.query(
-    selectUserInfoQuery,
-    selectUserInfoParams
-  );
-  return [userInfoRows];
-}
-
-module.exports = {
-  userEmailCheck,
-  userNicknameCheck,
-  insertUserInfo,
-  selectUserInfo,
-};
