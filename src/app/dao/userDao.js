c34e3837cfdcce582a6fd5040aae31927a001d72
sangm1n <dltkd96als@naver.com>
2021-01-18 00:18:11 +0900

    [Update] 회원가입 API 분리
    필수 정보 입력 / 추가 정보 입력 두 개의 API로 분리

--- src/app/dao/userDao.js
+++ src/app/dao/userDao.js
@@ -46,14 +46,14 @@ exports.checkUserNickname = async function (nickname) {
 
 // 사용자 정보 입력
 // 레벨 부여되도록 트랜잭션 처리 필요
-exports.postUserInfo = async function (name, email, password, nickname, height, weight, gender) {
+exports.postUserInfo = async function (name, email, password) {
     try {
         const connection = await pool.getConnection(async (conn) => conn);
         try {
             const query = `
-            insert into User (userName, email, password, nickname, height, weight, gender) values (?, ?, ?, ?, ?, ?, ?);
+            insert into User (userName, email, password) values (?, ?, ?);
             `
-            const params = [name, email, password, nickname, height, weight, gender];
+            const params = [name, email, password];
             const [rows] = await connection.query(
                 query, params
             );
@@ -66,6 +66,30 @@ exports.postUserInfo = async function (name, email, password, nickname, height, 
         }
     } catch (err) {
         logger.error(`App - postUserInfo DB Connection error\n: ${err.message}`);
+        return res.status(500).send(`Error: ${err.message}`);
+    }
+}
+
+exports.postUserAddInfo = async function (nickname, height, weight, gender, userId) {
+    try {
+        const connection = await pool.getConnection(async (conn) => conn);
+        try {
+            const query = `
+            update User set nickname = ?, height = ?, weight = ?, gender = ? where userId = ?;
+            `
+            const params = [nickname, height, weight, gender, userId];
+            const [rows] = await connection.query(
+                query, params
+            );
+            connection.release();
+        } catch (err) {
+            await connection.rollback();
+            connection.release();
+            logger.error(`App - postUserAddInfo Transaction error\n: ${err.message}`);
+            return res.status(500).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - postUserAddInfo DB Connection error\n: ${err.message}`);
         return res.status(500).send(`Error: ${err.message}`);
     }
 }
