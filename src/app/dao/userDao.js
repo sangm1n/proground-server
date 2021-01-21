21792d8c9d48684b4a85063c2fede47faac7494e
sangm1n <dltkd96als@gmail.com>
2021-01-21 22:15:00 +0900

    [Update] 회원가입 / 로그인 코드 보완
    새로운 resModule에 맞게 return 값 재설정

--- src/app/dao/userDao.js
+++ src/app/dao/userDao.js
@@ -45,49 +45,35 @@ exports.checkUserNickname = async function (nickname) {
 }
 
 // 사용자 정보 입력
-// 레벨 부여되도록 트랜잭션 처리 필요
 exports.postUserInfo = async function (name, email, password) {
     try {
         const connection = await pool.getConnection(async (conn) => conn);
-        try {
-            const query = `
-            insert into User (userName, email, password) values (?, ?, ?);
-            `
-            const params = [name, email, password];
-            const [rows] = await connection.query(
-                query, params
-            );
-            connection.release();
-        } catch (err) {
-            await connection.rollback();
-            connection.release();
-            logger.error(`App - postUserInfo Transaction error\n: ${err.message}`);
-            return res.status(500).send(`Error: ${err.message}`);
-        }
+        const query = `
+        insert into User (userName, email, password, loginStatus) values (?, ?, ?, "G");
+        `
+        const params = [name, email, password];
+        const [rows] = await connection.query(
+            query, params
+        );
+        connection.release();
     } catch (err) {
         logger.error(`App - postUserInfo DB Connection error\n: ${err.message}`);
         return res.status(500).send(`Error: ${err.message}`);
     }
 }
 
+// 사용자 추가 정보 입력
 exports.postUserAddInfo = async function (nickname, height, weight, gender, userId) {
     try {
         const connection = await pool.getConnection(async (conn) => conn);
-        try {
-            const query = `
-            update User set nickname = ?, height = ?, weight = ?, gender = ? where userId = ?;
-            `
-            const params = [nickname, height, weight, gender, userId];
-            const [rows] = await connection.query(
-                query, params
-            );
-            connection.release();
-        } catch (err) {
-            await connection.rollback();
-            connection.release();
-            logger.error(`App - postUserAddInfo Transaction error\n: ${err.message}`);
-            return res.status(500).send(`Error: ${err.message}`);
-        }
+        const query = `
+        update User set nickname = ?, height = ?, weight = ?, gender = ? where userId = ?;
+        `
+        const params = [nickname, height, weight, gender, userId];
+        const [rows] = await connection.query(
+            query, params
+        );
+        connection.release();
     } catch (err) {
         logger.error(`App - postUserAddInfo DB Connection error\n: ${err.message}`);
         return res.status(500).send(`Error: ${err.message}`);
