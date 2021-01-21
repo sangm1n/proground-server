69bc8f1623c469c9e7925af0d3efc1645672a418
sangm1n <dltkd96als@naver.com>
2021-01-21 22:15:00 +0900

    [Update] 회원가입 / 로그인 코드 보완
    새로운 resModule에 맞게 return 값 재설정

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -9,7 +9,7 @@ const secret_config = require('../../../config/secret');
 const userDao = require('../dao/userDao');
 const { constants } = require('buffer');
 
-const response = require('../controllers/responseController')
+const response = require('../../utils/response');
 
 /***
  * update : 2021-01-15
@@ -20,21 +20,21 @@ exports.signUp = async function (req, res) {
         name, email, password
     } = req.body;
 
-    if (!name) return response.status(res, "", "EMPTY_NAME");
-    if (!email) return response.status(res, "", "EMPTY_EMAIL");
-    if (!regexEmail.test(email)) return response.status(res, "", "INVALID_EMAIL");
-    if (!password) return response.status(res, "", "EMPTY_PASSWORD");
+    if (!name) return res.json(response.successFalse(2050, "이름을 입력해주세요."));
+    if (!email) return res.json(response.successFalse(2020, "이메일을 입력해주세요."));
+    if (!regexEmail.test(email)) return res.json(response.successFalse(2021, "이메일 형식을 확인해주세요."));
+    if (!password) return res.json(response.successFalse(2030, "비밀번호를 입력해주세요."));
 
     try {
         const emailRows = await userDao.checkUserEmail(email);
-        if (emailRows === 1) return response.status(res, "", "DUPLICATED_EMAIL");
+        if (emailRows === 1) return res.json(response.successFalse(3018, "이미 존재하는 이메일입니다."));
 
         const hashedPassword = await crypto.createHash('sha512').update(password).digest('hex');
         await userDao.postUserInfo(name, email, hashedPassword);
         const userInfoRows = await userDao.getUserInfo(email);
 
         const result = { userId: userInfoRows.userId }
-        return response.status(res, result, "SUCCESS_POST_USER");
+        return res.json(response.successTrue(1012, "회원가입에 성공하였습니다.", result));
     } catch (err) {
         logger.error(`App - signUp Query error\n: ${err.message}`);
         return res.status(500).send(`Error: ${err.message}`);
@@ -53,19 +53,19 @@ exports.signUpAdd = async function (req, res) {
         userId
     } = req.params;
 
-    if (!nickname) return response.status(res, "", "EMPTY_NICKNAME");
-    if (nickname.length > 7) return response.status(res, "", "INVALID_NICKNAME");
-    if (!height) return response.status(res, "", "EMPTY_HEIGHT");
-    if (!weight) return response.status(res, "", "EMPTY_WEIGHT");
-    if (!gender) return response.status(res, "", "EMPTY_GENDER");
+    if (!nickname) return res.json(response.successFalse(2040, "닉네임을 입력해주세요."));
+    if (nickname.length > 7) return res.json(response.successFalse(2041, "닉네임은 7자 미만으로 입력해주세요."));
+    if (!height) return res.json(response.successFalse(2042, "키를 입력해주세요."));
+    if (!weight) return res.json(response.successFalse(2043, "몸무게를 입력해주세요."));
+    if (!gender) return res.json(response.successFalse(2044, "성별을 입력해주세요."));
 
     try {
         const nicknameRows = await userDao.checkUserNickname(nickname);
-        if (nicknameRows === 1) return response.status(res, "", "DUPLICATED_NICKNAME");
+        if (nicknameRows === 1) return res.json(response.successTrue(3017, "이미 존재하는 닉네임입니다."));
 
         await userDao.postUserAddInfo(nickname, height, weight, gender, userId);
 
-        return response.status(res, "", "SUCCESS_POST_USER");
+        return res.json(response.successTrue(1012, "회원가입에 성공하였습니다."));
     } catch (err) {
         logger.error(`App - signUpAdd Query error\n: ${err.message}`);
         return res.status(500).send(`Error: ${err.message}`);
@@ -81,15 +81,15 @@ exports.logIn = async function (req, res) {
         email, password
     } = req.body;
 
-    if (!email) return response.status(res, "", "EMPTY_EMAIL");
-    if (!password) return response.status(res, "", "EMPTY_PASSWORD");
+    if (!email) return res.json(response.successFalse(2020, "이메일을 입력해주세요."));
+    if (!password) return res.json(response.successFalse(2030, "비밀번호를 입력해주세요."));
 
     try {
         const emailRows = await userDao.checkUserEmail(email);
         const userInfoRows = await userDao.getUserInfo(email);
         const hashedPassword = await crypto.createHash('sha512').update(password).digest('hex');
 
-        if (emailRows === 0 || hashedPassword !== userInfoRows.password) return response.status(res, "", "FAILED_TO_LOGIN");
+        if (emailRows === 0 || hashedPassword !== userInfoRows.password) return res.json(response.successFalse(3014, "이메일 주소 혹은 비밀번호가 일치하지 않습니다."));
 
         const token = await jwt.sign({
             userId: userInfoRows.userId
@@ -101,7 +101,7 @@ exports.logIn = async function (req, res) {
         });
 
         const result = { jwt: token };
-        return response.status(res, result, "SUCCESS_LOGIN");
+        return res.json(response.successTrue(1013, "로그인에 성공하였습니다.", result));
     } catch (err) {
         logger.error(`App - logIn Query error\n: ${JSON.stringify(err)}`);
         return false;
