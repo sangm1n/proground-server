f6fc0e1e0b975178b3c542aa97da5ed0280c4f77
sangm1n <dltkd96als@naver.com>
2021-02-16 23:37:54 +0900

    [Fix] nodemailer 앱 비밀번호 수정

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
     'jwtsecret' :  'softsquared_jwt_secret_key_07040014087',
     'ADMIN_EMAIL' : 'proground.developer@gmail.com',
-    'ADMIN_PASSWORD' : 'vmfh158!!'
+    'ADMIN_PASSWORD' : 'qvqr emil hvse xifp'
 };
