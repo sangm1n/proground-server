dec41c7c994d57dc10318481cf8e94577b230a20
sangm1n <dltkd96als@naver.com>
2021-03-06 16:40:00 +0900

    [Fix] 공지사항 관련 API method POST로 변경

--- src/app/routes/noticeRoute.js
+++ src/app/routes/noticeRoute.js
@@ -2,6 +2,6 @@ module.exports = function(app){
     const notice = require('../controllers/noticeController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-    app.route('/notices').get(notice.allNotices);
-    app.route('/notices/:noticeId').get(notice.noticeInfo);
+    app.route('/notices').post(notice.allNotices);
+    app.route('/notices/:noticeId').post(notice.noticeInfo);
 };
