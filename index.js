3d33e54bbf0d420fa277b7fdaa2f50be7ed25c13
sangm1n <dltkd96als@naver.com>
2021-03-08 01:00:57 +0900

    [Add] 미션 유효기간 (부여 후 14일) 스케줄링 구현

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const schedule = require('node-schedule');
 const runningDao = require('./src/app/dao/runningDao');
 const challengeDao = require('./src/app/dao/challengeDao');
 const userDao = require('./src/app/dao/userDao');
+const adminDao = require('./src/app/dao/adminDao');
 const notification = require('./src/utils/notification');
 
 let port;
@@ -27,10 +28,9 @@ schedule.scheduleJob('0 0 18 * * *', async function() {
             notification('[프로그라운드]', `헛둘헛둘! 오늘 ${countRows.runningCount}명이 달렸어요! 🏃🏻`, totalFcmRows[i].fcmToken);
         }
     }
-})
+});
 
 schedule.scheduleJob('0 0 0 * * *', async function() {
-    
     let now = new Date();
     let today = now.getFullYear()+ '-' + (now.getMonth()+1).toString().padStart(2,'0') + '-' + now.getDate().toString().padStart(2,'0');
     let yesterday = new Date(now.setDate(now.getDate() - 1));
@@ -49,7 +49,17 @@ schedule.scheduleJob('0 0 0 * * *', async function() {
         else await challengeDao.challengeResult(challengeId, 'N');
         logger.info(`${challengeId}번 챌린지 종료 - 승리 여부 저장 완료`);
     }
-})
+
+    const missionRows = await adminDao.getMissionInfo();
+    for (var i = 0; i < missionRows.length; i++) {
+        let userId = missionRows[i].userId;
+        let missionId = missionRows[i].missionId;
+        let createdAt = missionRows[i].createdAt;
+
+        await adminDao.updateUserMission(userId, missionId, createdAt);
+        logger.info(`${userId}번 사용자의 ${missionId}번 미션 14일 지남 - 삭제 완료`);
+    }
+});
 
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
