b6509fad9c395062219301e9c277502ca0fd569b
sangm1n <dltkd96als@naver.com>
2021-04-16 22:47:53 +0900

    [Fix] tracking 제거 및 .gitignore 반영

--- config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'jwtsecret' :  'softsquared_jwt_secret_key_07040014087',
-    'ADMIN_EMAIL' : 'proground.developer@gmail.com',
-    'ADMIN_PASSWORD' : 'qvqr emil hvse xifp'
-};
